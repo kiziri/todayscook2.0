932b8a26fa69b30c98886ff80a4980320b1c56b9
kiziri <ekelekeleh@naver.com>
2022-12-09 14:28:25 +0900

    기본 패키지 구조 및 파일 설정

---
+++ src/comp/view/common/navbar/NavbarIndex.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NavbarIndex = () => {
+  return(
+    <div>
+
+    </div>
+  );
+}
+export default NavbarIndex;
---
+++ src/comp/view/mainHome/MainHomeIndex.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MainHomeIndex = () => {
+  return(
+    <div>
+
+    </div>
+  );
+}
+export default MainHomeIndex;
